7979cdcbf7d306f6aedc5a43ad42ce4f6f7612c6
greenmg <greenmg0102@proton.me>
2024-03-05 15:13:32 -0500

    fixed issue in my post job post status

--- src/app/api/admin/job-post/job-post-status/route.ts
+++ src/app/api/admin/job-post/job-post-status/route.ts
@@ -4,14 +4,18 @@ import { connectToDatabase } from "@/lib/mongodb";
 import { currentUser } from '@clerk/nextjs';
 
 export async function GET(req: any, res: any) {
+  try {
+    let { db } = await connectToDatabase();
+    const user = await currentUser();
 
-  let { db } = await connectToDatabase();
-  const user = await currentUser();
+    let result = await db
+      .collection('myjobposts')
+      .findOne({ recruiterId: user.id, isComplete: false });
 
-  let result = await db
-    .collection('myjobposts')
-    .findOne({ recruiterId: user.id, isComplete: false });
-
-  return NextResponse.json(result);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ error: "An error occurred while processing the request" });
+  }
 
 }
