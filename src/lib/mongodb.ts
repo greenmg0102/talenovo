b79b5684d2cda08bc09253ff1f7a7443b3a4debc
greenmg <greenmg0102@proton.me>
2024-03-07 04:25:07 -0500

    change the password

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,8 +2,8 @@
 import { Db, MongoClient } from "mongodb";
 
 const MONGODB_DB = "talenovo";
-// const uri = "mongodb+srv://greenmg0102:ZnrVFGLqb8q6iv5E@cluster0.wj0ebxi.mongodb.net/?retryWrites=true&w=majority"
-const uri = "mongodb+srv://greenmg0102:ZnrVFGLqb8q6iv5E@cluster0.wj0ebxi.mongodb.net/?retryWrites=true&w=majority&ssl=false"
+// const uri = "mongodb+srv://greenmg0102:PFjEt0dbZLqN16YI@cluster0.wj0ebxi.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://greenmg0102:PFjEt0dbZLqN16YI@cluster0.wj0ebxi.mongodb.net/?retryWrites=true&w=majority&ssl=false"
 
 let cachedClient: MongoClient;
 let cachedDb: Db;
