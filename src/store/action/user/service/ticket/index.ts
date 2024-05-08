d8e2179238994f27f6b9e98796ecd182d9e22556
greenmg <greenmg0102@proton.me>
2024-05-08 07:52:50 -0400

    *&^%$#@

--- src/store/action/user/service/ticket/index.ts
+++ src/store/action/user/service/ticket/index.ts
@@ -1,7 +1,7 @@
 
 export async function sendingTicket(message: any) {
 
-  const res = await fetch('http://localhost:3000/api/service/ticket', {
+  const res = await fetch('http://104.128.55.140:3000/api/service/ticket', {
     method: 'POST',
     body: JSON.stringify(message)
   });
