81725a31ee653da6619ef8b18c3f3f0869e6e8de
greenmg <greenmg0102@proton.me>
2024-05-23 05:41:53 -0400

    @@@@

--- src/store/action/user/userProfile/userInfo.ts
+++ src/store/action/user/userProfile/userInfo.ts
@@ -1,20 +1,20 @@
 
 export async function userInitialInfo() {
-    const res = await fetch('http://localhost:3000/api/user/user-profile/user-info', {
+    const res = await fetch('http://104.128.55.140:3000/api/user/user-profile/user-info', {
         method: 'GET',
     });
     return await res.json();
 }
 
 export async function userPremiumStatus() {
-    const res = await fetch('http://localhost:3000/api/user/user-profile/user-premium-status', {
+    const res = await fetch('http://104.128.55.140:3000/api/user/user-profile/user-premium-status', {
         method: 'GET',
     });
     return await res.json();
 }
 
 export async function userBannerRegist(data: any) {
-    const res = await fetch('http://localhost:3000/api/user/user-profile/user-info/user-banner', {
+    const res = await fetch('http://104.128.55.140:3000/api/user/user-profile/user-info/user-banner', {
         method: 'POST',
         body: JSON.stringify(data)
     });
@@ -22,7 +22,7 @@ export async function userBannerRegist(data: any) {
 }
 
 export async function userOtherRegist(data: any) {
-    const res = await fetch('http://localhost:3000/api/user/user-profile/user-info/user-other', {
+    const res = await fetch('http://104.128.55.140:3000/api/user/user-profile/user-info/user-other', {
         method: 'POST',
         body: JSON.stringify(data)
     });
